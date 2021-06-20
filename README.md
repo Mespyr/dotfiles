ae8958b6a1627175cbe29b08aa39d0d15fc32dd2
Senuk Wijesinghe <senukcw@gmail.com>
2021-06-20 10:25:40 -0400

    Added install script

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
 # Config
 
-- [Alacritty](alacritty.yml)
+My config files.
+My AwesomeWM config is from the [awesome-copycats](https://github.com/lcpz/awesome-copycats) repo.
 
-## Applications
-
-- Cargo     - `sudo apt install cargo`
-- Exa       - `cargo install exa`
-- Alacritty - `cargo install alacritty`
-- Nautilus  - `sudo apt install nautilus`
-
-## GTK Theme
-- [Adapta Nokto GTK Theme](https://cinnamon-spices.linuxmint.com/themes/view/Adapta-Nokto)
-- [wallpaper.jpg](wallpaper.jpg) is from [Adapta Backgrounds](https://github.com/adapta-project/adapta-backgrounds)
-
-## VSCode Theme
-- The [AdaptaNokto](https://github.com/j-james/adapta-nokto-vscode) VSCode Color Theme.
-- The [Material](https://github.com/PKief/vscode-material-icon-theme) Icon Theme
-- The [Material](https://github.com/PKief/vscode-material-product-icons) Product Icon Theme
+# GTK Theme
+The [GruvBox Material GTK](https://github.com/sainnhe/gruvbox-material-gtk) Theme and Icons.
---
+++ install-theme.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# install theme system-wide, along with configs
+function install_system_wide() {
+    echo 
+    echo "Installing Theme and Icons system-wide."
+    echo
+
+    git clone https://github.com/sainnhe/gruvbox-material-gtk
+
+    # install theme
+    cp -r gruvbox-material-gtk/themes/* -t /usr/share/themes/
+
+    # install icons
+    cp -r gruvbox-material-gtk/icons/* -t /usr/share/icons/
+
+    echo 
+    echo "Installing Awesome Config." 
+    echo
+
+    cp -r awesome/ ~/.config
+
+    echo 
+    echo "Installing Alacritty Config." 
+    echo
+
+    cp -r alacritty/ ~/.config
+}
+
+function install_locally() {
+    echo 
+    echo "Installing Theme and Icons locally."
+    echo
+
+    git clone https://github.com/sainnhe/gruvbox-material-gtk
+
+    # install theme
+    mkdir ~/.themes
+    cp -r gruvbox-material-gtk/themes/* -t ~/.themes
+
+    # install icons
+    mkdir ~/.icons
+    cp -r gruvbox-material-gtk/icons/* -t ~/.icons
+
+    echo 
+    echo "Installing Awesome Config." 
+    echo
+
+    cp -r awesome/ ~/.config
+
+    echo 
+    echo "Installing Alacritty Config." 
+    echo
+
+    cp -r alacritty/ ~/.config
+
+}
+
+
+echo "Packages needed to install:"
+echo " - AwesomeWM (awesome)"
+echo " - Compton (compton) or Picom (picom)"
+echo " - Alacritty (alacritty): Default Terminal Emulator"
+echo
+
+echo "Recommended Packages:"
+echo " - lxappearance (lxappearance)"
+echo " - VSCode (code): Preferred Code Editer"
+echo
+
+# check if user wants to install
+printf "Install? (y/N): "
+read INSTALL_PERMISSIONS
+if [ "$INSTALL_PERMISSIONS" != "y" ]; then
+    if [ "$INSTALL_PERMISSIONS" != "Y" ]; then
+        echo "Aborting."
+        exit
+    fi
+fi
+
+# check if running with root permissions
+if [ `id -u` == 0 ]; then
+    install_system_wide
+    exit
+fi
+
+install_locally
+
