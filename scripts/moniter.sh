58aed970eb4542b5ce1045c5bbe418efe408b975
Mespyr <senukcw@gmail.com>
2021-08-08 20:47:02 -0400

    moniter script

--- scripts/moniter.sh
+++ scripts/moniter.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output VGA1 --brightness 0.7 --output LVDS1 --brightness 0.7
+xrandr --output LVDS1 --primary --mode 1366x768 --pos 1920x312 --rotate normal --output DP1 --off --output HDMI1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
