f95ef8be248f23963c7d46f6fe6d97521f6654ce
Mespyr <senukcw@gmail.com>
2021-11-09 15:27:12 -0500

    stuff

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -5,7 +5,6 @@ local awful = require("awful")
 local wibox = require("wibox")
 local dpi   = require("beautiful.xresources").apply_dpi
 local os = os
-local my_table = awful.util.table
 
 -- theme
 local theme = {}
@@ -27,10 +26,6 @@ theme.widget_bg = theme.bg_normal
 theme.border_width = dpi(3)
 theme.border_normal = "#33383B"
 theme.border_focus = "#edaf76"
--- theme.border_marked = theme.border_focus
--- theme.border_focus = theme.bg_focus
--- theme.titlebar_bg_focus = theme.border_focus
--- theme.titlebar_bg_normal = theme.border_normal
 -- Panel
 theme.panel_height = dpi(40)
 -- notifications
@@ -42,64 +37,18 @@ theme.taglist_bg_empty                          = theme.widget_bg
 theme.taglist_fg_empty                          = theme.bg_focus
 theme.taglist_bg_focus                          = theme.widget_bg
 theme.taglist_fg_focus                          = theme.border_focus
--- Icons
-theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
-theme.layout_tile                               = theme.dir .. "/icons/tile.png"
-theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
-theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
-theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
-theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
-theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
-theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
-theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
-theme.layout_max                                = theme.dir .. "/icons/max.png"
-theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
-theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
-theme.layout_floating                           = theme.dir .. "/icons/floating.png"
-theme.widget_ac                                 = theme.dir .. "/icons/ac.png"
-theme.widget_battery                            = theme.dir .. "/icons/battery.png"
-theme.widget_battery_low                        = theme.dir .. "/icons/battery_low.png"
-theme.widget_battery_empty                      = theme.dir .. "/icons/battery_empty.png"
-theme.widget_mem                                = theme.dir .. "/icons/mem.png"
-theme.widget_cpu                                = theme.dir .. "/icons/cpu.png"
-theme.widget_temp                               = theme.dir .. "/icons/temp.png"
-theme.widget_net                                = theme.dir .. "/icons/net.png"
-theme.widget_hdd                                = theme.dir .. "/icons/hdd.png"
-theme.widget_music                              = theme.dir .. "/icons/note.png"
-theme.widget_music_on                           = theme.dir .. "/icons/note_on.png"
-theme.widget_vol                                = theme.dir .. "/icons/vol.png"
-theme.widget_vol_low                            = theme.dir .. "/icons/vol_low.png"
-theme.widget_vol_no                             = theme.dir .. "/icons/vol_no.png"
-theme.widget_vol_mute                           = theme.dir .. "/icons/vol_mute.png"
-theme.widget_mail                               = theme.dir .. "/icons/mail.png"
-theme.widget_mail_on                            = theme.dir .. "/icons/mail_on.png"
-theme.widget_power_btn                          = theme.dir .. "/icons/power.png"
 
 -- widgets
 local markup = lain.util.markup
-
 local spr = wibox.widget.textbox('  ')
 local small_spr = wibox.widget.textbox(' ')
 
--- Textclock
 local clocktext = awful.widget.watch(
     "date +'%a %I:%M'", 60,
     function(widget, stdout)
         widget:set_markup(" " .. markup.font(theme.font, stdout))
     end
 )
-
-local clock = wibox.widget{
-	{
-        layout = wibox.layout.fixed.horizontal,
-        spr,
-        spr,
-        clocktext,
-        spr,
-        spr,
-    },
-	widget = wibox.container.background
-}
 
 local battery = lain.widget.bat({
     settings = function()
@@ -114,50 +63,14 @@ local battery = lain.widget.bat({
         end
 end})
 
-local bat = wibox.widget{
-	{
-        layout = wibox.layout.fixed.horizontal,
-        battery,
-        small_spr,
-        spr
-    },
-	widget = wibox.container.background
-}
-
 -- screen
 function theme.at_screen_connect(s)
     -- If wallpaper is a function, call it with the screen
     local wallpaper = theme.wallpaper
-    if type(wallpaper) == "function" then
-        wallpaper = wallpaper(s)
-    end
     gears.wallpaper.maximized(wallpaper, s, true)
 
     -- Tags
     awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
-
-    -- layout box
-    s.mylayoutbox = wibox.widget {
-    	{
-            layout = wibox.layout.fixed.horizontal,
-            spr,
-            small_spr,
-            wibox.container.margin(awful.widget.layoutbox(s), 7, 7, 7, 7),
-            small_spr,
-            spr,
-        },
-    	widget = wibox.container.background
-    }
-    s.mylayoutbox:buttons(my_table.join(
-                           awful.button({}, 1, function () awful.layout.inc( 1) end),
-                           awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
-                           awful.button({}, 3, function () awful.layout.inc(-1) end),
-                           awful.button({}, 4, function () awful.layout.inc( 1) end),
-                           awful.button({}, 5, function () awful.layout.inc(-1) end)))
-
-    -- local box = function(cr, width, height)
-    --     return gears.shape.rectangle(cr, width, height, 2)
-    -- end
 
     -- Create a taglist widget
     s.mytaglist = awful.widget.taglist {
@@ -171,20 +84,6 @@ function theme.at_screen_connect(s)
             spacing = 10,
             layout  = wibox.layout.fixed.horizontal
         }
-    }
-
-    s.mytaglist = wibox.widget {
-    	{
-            layout = wibox.layout.fixed.horizontal,
-            spr,
-            spr,
-            spr,
-            wibox.container.margin(s.mytaglist, 0, 0, 0, 0),
-            spr,
-            spr,
-            spr
-        },
-    	widget = wibox.container.background
     }
 
     -- Create the wibox
@@ -207,16 +106,24 @@ function theme.at_screen_connect(s)
         {
             layout = wibox.layout.fixed.horizontal,
             spr,
-            s.mytaglist,
-        },
-        {
-            layout = wibox.layout.fixed.horizontal,
-            spr
-        },
-        {
-            layout = wibox.layout.fixed.horizontal,
-            bat,
             spr,
+            spr,
+            spr,
+            s.mytaglist,
+            spr,
+            spr,
+            spr,
+        },
+        {
+            layout = wibox.layout.fixed.horizontal,
+            clocktext
+        },
+        {
+            layout = wibox.layout.fixed.horizontal,
+            battery,
+            spr,
+            spr,
+            small_spr
         }
     }
 end
