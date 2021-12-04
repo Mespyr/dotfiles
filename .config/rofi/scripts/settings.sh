7bd8d1219ab7c27c4e7ed51094c06a8cb4ccf3ec
Mespyr <senukcw@gmail.com>
2021-12-04 07:23:38 -0500

    add screenshot opt to seetings menu

--- .config/rofi/scripts/settings.sh
+++ .config/rofi/scripts/settings.sh
@@ -4,8 +4,9 @@ BACKLIGHT="   Backlight"
 RUN="   Run Menu"
 SOUND="   Sound"
 BLUETOOTH="   Bluetooth"
+SCREENSHOT="   Screenshot"
 
-OPTIONS="$POWERMENU\n$BACKLIGHT\n$RUN\n$SOUND\n$BLUETOOTH"
+OPTIONS="$POWERMENU\n$BACKLIGHT\n$RUN\n$SOUND\n$BLUETOOTH\n$SCREENSHOT"
 CHOSEN="$(echo -e "$OPTIONS" | rofi -dmenu -i \
     -config ~/.config/rofi/styles/settings.rasi )"
 
@@ -24,5 +25,9 @@ case $CHOSEN in
         ;;
     $BLUETOOTH)
         blueberry &
+        ;;
+    $SCREENSHOT)
+        scrot ~/screenshot.png -z &
+        notify-send "Screenshot saved to ~/screenshot.png" &
         ;;
 esac
