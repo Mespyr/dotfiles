1aa8fd9b3b495c80e144bc2966eda001112f1fb4
Mespyr <senukcw@gmail.com>
2022-09-19 21:57:13 -0400

    remove backlight script from rofi

--- .config/rofi/scripts/settings.sh
+++ .config/rofi/scripts/settings.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 POWERMENU="   power menu"
-BACKLIGHT="   backlight"
 RUN="   run menu"
 SOUND="   sound"
 BLUETOOTH="   bluetooth"
 SCREENSHOT="   screenshot"
 
-OPTIONS="$POWERMENU\n$BACKLIGHT\n$RUN\n$SOUND\n$BLUETOOTH\n$SCREENSHOT"
+OPTIONS="$POWERMENU\n$RUN\n$SOUND\n$BLUETOOTH\n$SCREENSHOT"
 CHOSEN="$(echo -e "$OPTIONS" | rofi -dmenu -i \
     -config ~/.config/rofi/styles/settings.rasi )"
 
 case $CHOSEN in 
     $POWERMENU)
         $HOME/.config/rofi/scripts/powermenu.sh
-        ;;
-    $BACKLIGHT)
-        $HOME/.config/rofi/scripts/backlight.py
         ;;
     $RUN)
         $HOME/.config/rofi/scripts/run.sh
